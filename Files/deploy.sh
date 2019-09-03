ccdf403428f70025f2329ddf7cc62a615464dd9e
Jhum0407 <Jhum0407@yahoo.com>
2019-09-03 10:28:40 -0400

    small change

---
+++ Files/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# enter your function code here
+BASE_DIR="c/Users/Jelehu/Documents/java"
+SOURCE_DIR="$BASE_DIR/simple-java-maven-app"
+TARGET_DIR="$SOURCE_DIR/Files"
+ARCHIVE_DIR="$BASE_DIR/Files"
+
+mkdir -p $ARCHIVE_DIR
+
+echo $BASE_DIR
+echo $SOURCE_DIR
+echo $TARGET_DIR
+echo "WIP"
